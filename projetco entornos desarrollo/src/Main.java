57bb62b1413cb878ac83b3ea7c46fd46776d5eb3
sagar <jonsagar.crespo@ikasle.egibide.org>
2024-11-28 20:58:51 +0100

    registrar y listar

--- projetco entornos desarrollo/src/Main.java
+++ projetco entornos desarrollo/src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static final int MAX_TAREAS = 10;
+    static String[] titulos = new String[MAX_TAREAS];
+    static String[] descripciones = new String[MAX_TAREAS];
+    static String[] fechasVencimiento = new String[MAX_TAREAS];
+    static int contador = 0;
+
+
+    public static void main(String[] args) throws IOException {
+       //write your code
+    }
+
+
+    public static void registrarTarea(String titulo, String descripcion, String fechaVencimiento) {
+        if (contador < MAX_TAREAS) {
+            titulos[contador] = titulo;
+            descripciones[contador] = descripcion;
+            fechasVencimiento[contador] = fechaVencimiento;
+            contador++;
+            System.out.println("Tarea '" + titulo + "' registrada.");
+        } else {
+            System.out.println("No se pueden registrar más tareas. Límite alcanzado.");
         }
     }
+
+    public static void listarTareas() {
+        if (contador == 0) {
+            System.out.println("No hay tareas registradas.");
+        } else {
+            for (int i = 0; i < contador; i++) {
+                System.out.println("Título: " + titulos[i] + ", Descripción: " + descripciones[i] + ", Fecha de vencimiento: " + fechasVencimiento[i]);
+            }
+        }
+    }
+
+
 }
